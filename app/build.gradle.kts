70a7f3a523c31cba887c3502e85d53a31f008590
Bogdan <yakogdan@gmail.com>
2025-05-20 21:35:05 +0800

    В AndroidManifest удалён ненужный атрибут android:label у Activity. Версия Java в модуле поднята по 17 версии. TargetApi поднят до 35 версии

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.example.newsaggregator"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
