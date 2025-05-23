cc79273e40d2093a28b51940d67560889323cf03
Bogdan <yakogdan@gmail.com>
2025-05-24 00:02:24 +0800

    При ошибке загрузки новостей показывается текст с ошибкой и кнопка для повторной загрузки новостей.

--- app/src/main/java/com/example/newsaggregator/ui/screens/news/NewsScreenContent.kt
+++ app/src/main/java/com/example/newsaggregator/ui/screens/news/NewsScreenContent.kt
@@ -1,11 +1,17 @@
 package com.example.newsaggregator.ui.screens.news
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,8 +19,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.example.newsaggregator.R
 import com.example.newsaggregator.ui.navigation.Screen
 
 @Composable
@@ -39,7 +51,10 @@ fun NewsScreenContent(
             }
 
             is NewsScreenState.Error -> {
-                ErrorState(message = state.throwable.message.toString())
+                ErrorState(
+                    message = state.throwable.message.toString(),
+                    onButtonClick = { newsViewModel.loadNews() },
+                )
             }
 
             is NewsScreenState.Success -> {
@@ -56,13 +71,6 @@ fun NewsScreenContent(
 private fun LoadingState() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         CircularProgressIndicator()
-    }
-}
-
-@Composable
-private fun ErrorState(message: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = message, color = MaterialTheme.colorScheme.error)
     }
 }
 
@@ -87,3 +95,47 @@ private fun SuccessState(
         }
     }
 }
+
+@Composable
+private fun ErrorState(
+    message: String,
+    onButtonClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            modifier = Modifier.wrapContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = message.toLoadingError(),
+                color = MaterialTheme.colorScheme.error,
+                maxLines = 6,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 20.sp,
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = { onButtonClick() }
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = stringResource(R.string.retry),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    fontSize = 16.sp,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun String.toLoadingError() =
+    LocalContext.current.getString(R.string.error_while_loading_news, this)
